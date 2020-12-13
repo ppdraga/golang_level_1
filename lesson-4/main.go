147e32afda742f36f06e16f0353ef15bde0748d8
Yaroslav <ppdraga@mail.ru>
2020-12-13 17:53:15 +0300

    fibonachi

---
+++ lesson-4/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var n_str string
+	fmt.Print("Input number: ")
+	fmt.Scanln(&n_str)
+
+	num, err := strconv.ParseInt(n_str, 10, 32)
+	if err != nil {
+		fmt.Println(n_str, "is not a number", err)
+		os.Exit(1)
+	}
+
+	result := fibonachi(1, 1, num)
+	fmt.Println(result)
+
+	cached_fibonachi := cached(fibonachi)
+	cached_fibonachi(1, 1, 7)
+	cached_fibonachi(1, 1, 7)
+	cached_fibonachi(1, 1, 7)
+	cached_fibonachi(1, 1, 8)
+	cached_fibonachi(1, 1, 8)
+}
+
+func fibonachi(a, b, n int64) int64 {
+	if n == 1 {
+		return a
+	} else if n == 2 {
+		return b
+	} else {
+		return fibonachi(a, b, n-2) + fibonachi(a, b, n-1)
+	}
+}
+
+func cached(f func(a, b, n int64) int64) func(a, b, n int64) int64 {
+	type args struct {
+		a int64
+		b int64
+		n int64
+	}
+	cache := make(map[args]int64)
+	return func(a, b, n int64) int64 {
+		f_args := args{a, b, n}
+		if val, ok := cache[f_args]; ok {
+			fmt.Println("from cache")
+			return val
+		} else {
+			result := f(a, b, n)
+			fmt.Println("to cache")
+			cache[f_args] = result
+			return result
+		}
+	}
+}
