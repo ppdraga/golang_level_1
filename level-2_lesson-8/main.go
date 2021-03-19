0a99366129b60f9d5420cc3ad0cfe14b48f4f0d4
Yaroslav <ppdraga@gmail.com>
2021-03-19 10:42:53 +0300

    init2

--- level-2_lesson-8/main.go
+++ level-2_lesson-8/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+var (
+	path  *string
+	force *bool
+	dir   string
+	err   error
+)
+
+func init() {
+	path = flag.String("path", ".", "Path where to search file duplicates")
+	force = flag.Bool("force", false, "Delete file duplicates")
+	flag.Parse()
+}
+
+func main() {
+	//var path = flag.String("path", ".", "Path where to search file duplicates")
+	fmt.Println(*path)
+	fmt.Println(*force)
+	if *path == "." {
+		dir, err = os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println(dir)
 }
